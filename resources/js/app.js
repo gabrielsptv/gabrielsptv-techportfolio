849ee484f107ea7e6ecca11c2ea2fec70493909b
gabrielsptv <gabrielbrasilioribeiroromano@gmail.com>
2024-07-20 23:56:39 -0300

    Initial commit with project setup

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+// Importe o Bootstrap JavaScript
+import 'bootstrap';
+
+// Importe o Bootstrap CSS (se ainda não estiver importado via CSS)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importe o jQuery (se necessário)
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <h1 class="display-4 fw-bold">Bem-vindo ao Laravel com Bootstrap</h1>
+        <p class="lead">Este é aaaaaaaaaum exemplo simples de integração da navbar Bootstrap no Laravel.</p>
+    </div>
+@endsection
